eb71750a64b5aca372bc468eef42e2714c94a22b
LEGO <19424005@student.hcmus.edu.vn>
2020-11-20 11:02:58 +0700

    init finished

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const db = require('./utils/db');
- 
-app.get('/', async function (req, res) {
-    var x = await db.deleteMany("Users", {name: "Hulk"}, {name: "H"});
-    console.log(x);
-    res.send("ok");
-});
+
+app.use('/test', require('./routes/test.route'));
  
 app.listen(3000);
+
+// error handler
+app.use((req, res, next) => {
+    
+});
