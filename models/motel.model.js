d1fd84c1b275bd6b2f3a9ccdb6c4637acd5f1fa8
LEGO <19424005@student.hcmus.edu.vn>
2020-12-06 15:31:27 +0700

    query motel, get single motel with owner info

--- models/motel.model.js
+++ models/motel.model.js
@@ -1,5 +1,8 @@
-const { ObjectId } = require('mongodb');
+const {
+  ObjectId
+} = require('mongodb');
 const db = require('../utils/db');
+const helper = require('../utils/helper');
 const TableName = 'Motels';
 
 module.exports = {
@@ -7,12 +10,65 @@ module.exports = {
     return db.find(TableName);
   },
   Single: (id) => {
-    return db.find(TableName, {
-      _id: ObjectId(`${id}`),
-    });
+    var aggregate = [{
+      $match: {
+        _id: ObjectId(`${id}`),
+      }
+    }, {
+      $lookup: {
+        from: 'Users',
+        localField: 'owner_id',
+        foreignField: '_id',
+        as: 'Users',
+      },
+    } ];
+    return db.aggregate(TableName, aggregate);
   },
-  GetPaginate: (start, limit) => {
-    return db.paginate(TableName, {}, { name: 1 }, start, limit);
+  GetCustom: (params) => {
+    var sort_object = {};
+    if (params.sortcol && params.sortdir) sort_object = JSON.parse(`{"${params.sortcol}": ${params.sortdir=='asc' ? 1 : 0}}`);
+
+    var query_object = {};
+    var query_address = '';
+    if (params.city) query_address = params.city;
+    if (params.district) query_address = "Quận " + params.district + (query_address ? ', ' + query_address : '');
+    if (query_address) query_object.address = new RegExp(query_address, 'i');
+    if (params.area) {
+      var range = params.area.split('-');
+      query_object.area = {
+        $gte: +range[0],
+        $lte: +range[1]
+      };
+    }
+    if (params.price) {
+      var range = params.price.split('-');
+      query_object.price = {
+        $gte: +range[0],
+        $lte: +range[1]
+      };
+    }
+    if (params.has_furniture) query_object.has_furniture = params.has_furniture == 'true';
+    if (params.is_verified) query_object.is_verified = params.is_verified == 'true';
+    if (params.searchkey) {
+      query_object.title = new RegExp(params.searchkey, 'i');
+      query_object.description = new RegExp(params.searchkey, 'i');
+    }
+
+    var aggregate = [];
+    if (!helper.ObjectIsEmpty(sort_object)) aggregate.push({
+      $sort: sort_object
+    });
+    if (!helper.ObjectIsEmpty(query_object)) aggregate.push({
+      $match: query_object
+    });
+    if (params.limit) aggregate.push({
+      $limit: +params.limit
+    });
+    if (params.offset) aggregate.push({
+      $skip: +params.offset
+    });
+
+    return db.aggregate(TableName, aggregate);
   },
   Add: (obj) => {
     obj.created_date = obj.modified_date = new Date();
@@ -21,14 +77,15 @@ module.exports = {
   Update: (id, obj) => {
     obj.modified_date = new Date();
     return db.updateOne(
-      TableName,
-      {
+      TableName, {
         _id: ObjectId(`${id}`),
       },
       obj
     );
   },
   Delete: (id) => {
-    return db.deleteOne(TableName, { _id: ObjectId(`${id}`) });
+    return db.deleteOne(TableName, {
+      _id: ObjectId(`${id}`)
+    });
   },
 };
